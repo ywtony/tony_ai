0d7c4ea406ccdf418b8677ddb917b6074b67cabe
wei.yang <wei.yang@wedo99.com>
2020-12-12 11:10:29 +0800

    1.实现基于图像平均值的二值分割

--- app/src/main/java/com/tony/ced/ui/MatPixelOptionActivity.java
+++ app/src/main/java/com/tony/ced/ui/MatPixelOptionActivity.java
@@ -1,8 +1,10 @@
 package com.tony.ced.ui;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -14,8 +16,9 @@ import com.tony.ced.R;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.imgproc.Imgproc;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +38,7 @@ import butterknife.ButterKnife;
  * @Version: 1.0
  */
 public class MatPixelOptionActivity extends BaseActivity {
+    private static final String TAG = "MatPixelOptionActivityTAG";
     @BindView(R.id.btnSelectImage)
     Button btnSelectImage;
     @BindView(R.id.ivImage)
@@ -63,6 +67,7 @@ public class MatPixelOptionActivity extends BaseActivity {
         datas.add("显示通道一的数据");
         datas.add("显示通道二的数据");
         datas.add("显示通道三的数据");
+        datas.add("均值与标准方差");
         show(datas);
     }
 
@@ -89,6 +94,9 @@ public class MatPixelOptionActivity extends BaseActivity {
                 break;
             case 6:
                 showSingleChannels(2);
+                break;
+            case 7:
+                optionMeanStd();
                 break;
         }
 
@@ -232,12 +240,13 @@ public class MatPixelOptionActivity extends BaseActivity {
         Utils.matToBitmap(mats.get(1), bitmap);
         ivImage.setImageBitmap(bitmap);
     }
-     /**
-      * @description 取单独的一个通道的图像数据并显示出来
-      * @date: 2020/12/12 10:36
-      * @author: wei.yang
-      */
-    private void showSingleChannels(int index){
+
+    /**
+     * @description 取单独的一个通道的图像数据并显示出来
+     * @date: 2020/12/12 10:36
+     * @author: wei.yang
+     */
+    private void showSingleChannels(int index) {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.girl5);
         Mat target = new Mat();
         Utils.bitmapToMat(bitmap, target);
@@ -245,5 +254,55 @@ public class MatPixelOptionActivity extends BaseActivity {
         Core.split(target, mats);
         Utils.matToBitmap(mats.get(index), bitmap);
         ivImage.setImageBitmap(bitmap);
+    }
+
+    /**
+     * @description 均值与标准方差 ：根据平均值可以实现基于平均值的图像二值分割，根据标准方差可以找到空白图像或者无效图像
+     * ps:如果stddevsArray[0]<5 那么图像基本上可以看成是无效图像或空白图像了，因为标准方差越小说明图像各个像素的差异越小，图像携带的有效信息就越小。我们可以通过这个结论
+     * 过滤质量不高的打印或扫描图像。
+     * @date: 2020/12/12 10:45
+     * @author: wei.yang
+     */
+    @SuppressLint("LongLogTag")
+    private void optionMeanStd() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.girl5);
+        Mat target = new Mat();
+        Utils.bitmapToMat(bitmap, target);
+        //转为灰度图像
+        Mat grayMat = new Mat();
+        Imgproc.cvtColor(target, grayMat, Imgproc.COLOR_BGR2GRAY);
+        //计算均值与标准方差
+        MatOfDouble means = new MatOfDouble();
+        MatOfDouble stddevs = new MatOfDouble();
+        Core.meanStdDev(grayMat, means, stddevs);
+        //显示均值与标准方差
+        double[] meansArray = means.toArray();
+        double[] stddevsArray = means.toArray();
+        Log.e(TAG, "均值：" + meansArray.toString());
+        Log.e(TAG, "标准方差：" + stddevsArray.toString());
+
+
+        //读取像素数组
+        int width = grayMat.cols();
+        int height = grayMat.rows();
+        byte[] data = new byte[width * height];
+        grayMat.get(0, 0, data);
+        int pv = 0;
+        //根据均值进行二值分割
+        int t = (int) meansArray[0];
+        for (int i = 0; i < data.length; i++) {
+            pv = data[i] & 0xff;
+            if (pv > t) {
+                data[i] = (byte) 255;
+            } else {
+                data[i] = (byte) 0;
+            }
+        }
+        grayMat.put(0,0,data);
+
+
+        Utils.matToBitmap(grayMat, bitmap);
+        ivImage.setImageBitmap(bitmap);
+
     }
 }
