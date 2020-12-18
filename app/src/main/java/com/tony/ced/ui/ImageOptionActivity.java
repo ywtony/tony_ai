74cf4d6ca1a798fcb4ae7b5690451034b26e01df
wei.yang <wei.yang@wedo99.com>
2020-12-18 09:27:57 +0800

    高斯模糊和中值滤波实现样例

--- app/src/main/java/com/tony/ced/ui/ImageOptionActivity.java
+++ app/src/main/java/com/tony/ced/ui/ImageOptionActivity.java
@@ -42,6 +42,7 @@ public class ImageOptionActivity extends BaseActivity {
     Button btnSelectImage;
     @BindView(R.id.ivImage)
     ImageView ivImage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,23 +50,33 @@ public class ImageOptionActivity extends BaseActivity {
         ButterKnife.bind(this);
         initViews();
     }
+
     private void initViews() {
         btnSelectImage.setOnClickListener(v -> {
             selectedImage();
         });
     }
+
     private void selectedImage() {
         List<String> datas = new ArrayList<>();
         datas.add("均值模糊");
+        datas.add("高斯模糊");
+        datas.add("中值滤波");
         show(datas);
     }
 
     @Override
     public void onMenuItemClick(int position) {
         super.onMenuItemClick(position);
-        switch (position){
+        switch (position) {
             case 0://均值模糊
                 toBlur();
+                break;
+            case 1://高斯模糊
+                toGaussianBlur();
+                break;
+            case 2://中值滤波
+                toMedianBlur();
                 break;
         }
     }
@@ -78,14 +89,70 @@ public class ImageOptionActivity extends BaseActivity {
      * ksize:图像卷积核大小
      * anchor:卷积核的中心位置
      * borderType:边缘填充类型，默认情况下是BORDER_DEFAULT
-     *
      */
-    private void toBlur(){
+    private void toBlur() {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.girl5);
         Mat target = new Mat();
         Utils.bitmapToMat(bitmap, target);
         Mat dst = new Mat();
-        Imgproc.blur(target,dst,new Size(5,5),new Point(-1,-1),Core.BORDER_DEFAULT);
+        Imgproc.blur(target, dst, new Size(5, 5), new Point(-1, -1), Core.BORDER_DEFAULT);
+        Utils.matToBitmap(dst, bitmap);
+        ivImage.setImageBitmap(bitmap);
+        target.release();
+        dst.release();
+    }
+
+    /**
+     * 高斯模糊：根据空间相对位置的不同，，卷积核中每个系数具有不同的系数（权重），因为我们经常使用高斯正态分布图的方式生成权重系数，所以这种模糊又称为高斯模糊
+     * 对于整个卷积核来说，越是在卷积核的中心地方，其系数应该越高，越接近边缘的地方，其系数应该越低，由此即可得知基于高斯的卷积核权重与空间位置之间的关系。
+     * GuassianBlur(Mat src,Mat dst,Size ksize,double sigmaX,double sigmaY,int borderType)
+     * src:表示输入图像
+     * dst:高斯模糊后的输出图像
+     * ksize：表示卷积核的大小，当ksize=new Size(0,0) 的时候表示从sigmaY计算得到
+     * sigmaX:  x方向的模糊程度
+     * sigmaY:y方向的模糊程度,当啊sigmaY不填写的时候表示从sigmaX计算得到
+     * borderType:表示边缘填充方式，默认为BORDER_DEFAULT
+     * <p>
+     * 高斯滤波常用于图像的预处理，能够起到很好的抑制噪声的作用，使用他们选择的卷积核大小通常为，3x3 ,5x5
+     * <p>
+     * 卷积核系数越大，模糊程度越高
+     */
+    private void toGaussianBlur() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.girl5);
+        Mat target = new Mat();
+        Utils.bitmapToMat(bitmap, target);
+        Mat dst = new Mat();
+        Imgproc.GaussianBlur(target, dst, new Size(5, 5), 0);
+        Utils.matToBitmap(dst, bitmap);
+        ivImage.setImageBitmap(bitmap);
+        target.release();
+        dst.release();
+    }
+
+    /**
+     * 统计排序滤波：中值滤波、最大值与最小值滤波
+     * 经常用来消除图像噪声或者抑制图像像素极小值与极大值
+     */
+    /**
+     * @description 中值滤波：同样也需要一个卷积核，与卷积滤波不同的是，其不会用卷积核的每个系数与对应像素值做算数运算，而是把对应的像素做排序，取中间值作为输出。
+     * @date: 2020/12/18 9:15
+     * @author: wei.yang
+     * <p>
+     * medianBlur(Mat src,Mat dst,int ksize)
+     * src：表示输入图像，当ksize为3、5的时候输入图像可以为浮点数或者整数，当ksize大于5时只能为字节类型，及CV_8UC
+     * dst:表示中值滤波后的输出图像，其类型与输入图像保持一致。
+     * ksize:表示模块大小，常见的是3、5，模板大小必须为奇数，而且必须大于1
+     *
+     *
+     * 特点：中值滤波对图像的椒盐噪声有很好的抑制作用，是一个很好的图像降噪滤波器
+     * 椒盐噪声：图片中随机出现的白点或者黑点
+     */
+    private void toMedianBlur() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.girl5);
+        Mat target = new Mat();
+        Utils.bitmapToMat(bitmap, target);
+        Mat dst = new Mat();
+        Imgproc.medianBlur(target, dst, 5);
         Utils.matToBitmap(dst, bitmap);
         ivImage.setImageBitmap(bitmap);
         target.release();
